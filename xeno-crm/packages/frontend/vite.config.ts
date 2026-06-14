e856c56c00aff81a974b40660518412f14f5915f
Krithik kumar M <krithikkumar2006@gmail.com>
2026-06-14 22:52:02 +0530

    Update vite.config.ts

--- xeno-crm/packages/frontend/vite.config.ts
+++ xeno-crm/packages/frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://crm-project-6r65.vercel.app/',
         changeOrigin: true,
       },
     },
